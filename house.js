df33738ee0048b4e3b330e01555f827f48d18281
Rudy Wahjudi <rudy.wahjudi@gmail.com>
2017-04-04 16:11:25 +0800

    done

--- house.js
+++ house.js
@@ -65,7 +65,9 @@ class House {
 }
 
 // const cool = new House('address', 100, 2, 2, 12345, 12345, true, true)
-const cool = new House({address: 'address', square_feet: 100, num_bedrooms: 2, num_baths: 2, cost: 12345, down_payment: 12345, sold: true, short_sale: true, has_tenants: true});
+const cool = new House(
+  {address: 'jalan sultan', square_feet: 100, num_bedrooms: 2, num_baths: 2, cost: 12345, down_payment: 12345, sold: true, short_sale: true, has_tenants: true}
+);
 
 console.log(cool.to_s())
 
